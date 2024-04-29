cb7d5d188afa336b5f69172fef26a10b310e910b
Siarhei Naralenkau <naralenkov2010@gmail.com>
2024-04-29 12:04:42 +0200

    Adding displaying of expenses in a table

--- src/pages/ExpensesTable.jsx
+++ src/pages/ExpensesTable.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import { Table, TableBody, TableCell, TableHeader, TableHeaderCell, TableRow } from "semantic-ui-react";
 
  function ExpenseTable() {
 
@@ -20,6 +21,27 @@ import { Link } from "react-router-dom";
     return (
         <>
             <p>Expenses should be displayed here as Table!</p>
+            <Table celled>
+                <TableHeader>
+                    <TableRow>
+                        <TableHeaderCell>Title</TableHeaderCell>
+                        <TableHeaderCell>Amount(EUR)</TableHeaderCell>
+                        <TableHeaderCell>Category</TableHeaderCell>
+                        <TableHeaderCell>Expense Date</TableHeaderCell>
+                    </TableRow>
+                </TableHeader>
+
+                <TableBody>
+                    {expenses.map( (expense) => (
+                        <TableRow key={expense.ID}>
+                            <TableCell>{expense.Title}</TableCell>
+                            <TableCell>{expense.Amount}</TableCell>
+                            <TableCell>{expense.Category}</TableCell>
+                            <TableCell>{expense.ExpenseDate}</TableCell>
+                        </TableRow>
+                    ))}    
+                </TableBody>
+            </Table>
             <p><Link to="/addExpense">Add New Expense</Link></p>
         </>
     );
